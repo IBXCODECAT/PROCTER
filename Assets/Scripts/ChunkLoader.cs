d1904391c8585353af5028852340ab430ce2dbb6
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-03-01 17:43:05 -0600

    Now We Can Load Or Generate

--- Assets/Scripts/ChunkLoader.cs
+++ Assets/Scripts/ChunkLoader.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class ChunkLoader : MonoBehaviour
 {
 	public Transform parent;
-	public float renderDistance;
+	public int renderDistance;
 	public float updateSpeed;
 
 	public Transform reference;
@@ -13,53 +13,111 @@ public class ChunkLoader : MonoBehaviour
 	public static Vector2 viewerPosition;
 
 	public static MapGenerator mapGenerator;
-	private int chunkSize;
+	private int chunkDiameter;
 
 	public float elevation = 0f;
 
-	private	static List<GameObject> loadedChunks = new List<GameObject>();
+	private int chunksGenerated = 0;
 
-	IEnumerator Start()
+	private	static List<GameObject> loadedChunks = new List<GameObject>();
+	private static List<GameObject> unLoadedChunks = new List<GameObject>();
+
+	private bool[,] ChunkPosFetchable;
+
+	private void Start()
     {
-		ChunkUpdate();
-		yield return new WaitForSecondsRealtime(updateSpeed);
+		StartCoroutine(Updater());
     }
 
-	private void ChunkUpdate()
+	IEnumerator Updater()
     {
-		for(float x = -renderDistance + viewerPosition.x; x < renderDistance + viewerPosition.x; x++)
+		while(true)
         {
-			for(float z = -renderDistance + viewerPosition.y; z < renderDistance + viewerPosition.y; z++)
-            {
-				float xPos = x * chunkSize;
-				float zPos = z * chunkSize;
+			ChunkSystem();
+			yield return new WaitForSecondsRealtime(updateSpeed);
+		}
+    }
 
-				loadedChunks.Add(GenerateChunk(new Vector3(xPos, elevation, zPos)));
+	private void ChunkSystem()
+    {
+
+		int viewerPositionX = Mathf.RoundToInt(viewerPosition.x);
+		int viewerPositionZ = Mathf.RoundToInt(viewerPosition.y);
+
+		GenerateNewChunks(viewerPositionX, viewerPositionZ);
+
+		UpdateChunkStates();
+    }
+
+	private void GenerateNewChunks(int xAprox, int zAprox)
+	{
+		for (int x = -renderDistance + xAprox; x < renderDistance + xAprox; x++)
+		{
+			for (int z = -renderDistance + zAprox; z < renderDistance + zAprox; z++)
+			{
+				int posX = Mathf.RoundToInt(x * chunkDiameter);
+				int posZ = Mathf.RoundToInt(z * chunkDiameter);
+
+				if (!ChunkPosFetchable[posX, posZ])
+                {
+					Debug.Log("Generated New Chunk");
+					loadedChunks.Add(GenerateChunk(new Vector3(posX, elevation, posZ)));
+
+					ChunkPosFetchable[posX, posZ] = true;
+				}
+			}
+		}
+	}
+
+	private void UpdateChunkStates()
+    {
+		foreach (GameObject chunk in loadedChunks)
+        {
+			if(Vector3.Distance(viewerPosition, chunk.transform.position) > renderDistance)
+            {
+				Dump(chunk);
+				Debug.Log("Dumped Chunk " + chunk, chunk);
+            }
+        }
+
+		foreach(GameObject chunk in unLoadedChunks)
+        {
+			if(Vector3.Distance(viewerPosition, chunk.transform.position) < renderDistance)
+            {
+				Load(chunk);
+				Debug.Log("Loaded Chunk " + chunk, chunk);
             }
         }
     }
 
 	private GameObject GenerateChunk(Vector3 pos)
-    {
-		Terrain activeChunk = Instantiate(new Terrain(), pos, Quaternion.identity, parent);
-		TerrainData chunkData = activeChunk.terrainData;
-		GameObject chunkObject = activeChunk.gameObject;
+	{
+		TerrainData chunkData = new TerrainData();
+		chunkData.name = "Chunk Data (" + chunksGenerated + ")";
+		GameObject chunkObject = Terrain.CreateTerrainGameObject(chunkData);
 		MapGenerator chunkGenerator = chunkObject.AddComponent<MapGenerator>();
 
+		chunkGenerator.data = chunkData;
+
 		chunkGenerator.GenerateMesh();
-		chunkSize = chunkGenerator.mapChunkSize;
+		chunkDiameter = chunkGenerator.mapChunkSize;
 
 		return chunkObject;
 	}
 
-	private void LoadChunk()
+	private void Load(GameObject chunk)
     {
-
+		loadedChunks.Add(chunk);
+		unLoadedChunks.Remove(chunk);
+		ChunkPosFetchable[Mathf.RoundToInt(chunk.transform.position.x), Mathf.RoundToInt(chunk.transform.position.z)] = true;
+		chunk.SetActive(true);
     }
 
-	private void UnloadChunk()
-    {
-
-    }
+	private void Dump(GameObject chunk)
+	{
+		loadedChunks.Remove(chunk);
+		unLoadedChunks.Add(chunk);
+		chunk.SetActive(false);
+	}
 	
 }
