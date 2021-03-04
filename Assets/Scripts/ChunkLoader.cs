96a551ce9b879008aebdae8967fc8242eba4c57f
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-03-03 19:25:27 -0600

    Update Script Vocab

--- Assets/Scripts/ChunkLoader.cs
+++ Assets/Scripts/ChunkLoader.cs
@@ -4,83 +4,77 @@ using System.Collections.Generic;
 
 public class ChunkLoader : MonoBehaviour {
 	public float renderDistance;
-	public Transform viewer;
-	[SerializeField] private GameObject chunk;
+	public Transform centerReference;
+	[SerializeField] private GameObject chunkObject;
 
 	[SerializeField] private short chunkSize;
 
-	public static Vector2 viewerPosition;
-	int chunksVisableInRenderDistance;
+	public static Vector2 center;
+	int chunksLoaded;
 
 	Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
 	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
-	void Start() {
-		chunksVisableInRenderDistance = Mathf.RoundToInt(renderDistance / chunkSize);
-	}
+	void Start() { chunksLoaded = Mathf.RoundToInt(renderDistance / chunkSize); }
 
-	void Update() {
-		viewerPosition = new Vector2 (viewer.position.x, viewer.position.z);
-		UpdateVisibleChunks ();
+	void Update()
+	{
+		center = new Vector2 (centerReference.position.x, centerReference.position.z);
+		UpdateRenderedOnly();
 	}
 		
-	void UpdateVisibleChunks() {
+	void UpdateRenderedOnly()
+	{
+		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) { terrainChunksVisibleLastUpdate [i].Loader (false); }
 
-		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) {
-			terrainChunksVisibleLastUpdate [i].SetVisible (false);
-		}
 		terrainChunksVisibleLastUpdate.Clear ();
 			
-		int currentChunkCoordX = Mathf.RoundToInt (viewerPosition.x / chunkSize);
-		int currentChunkCoordY = Mathf.RoundToInt (viewerPosition.y / chunkSize);
+		int currentChunkCoordX = Mathf.RoundToInt (center.x / chunkSize);
+		int currentChunkCoordY = Mathf.RoundToInt (center.y / chunkSize);
 
-		for (int yOffset = -chunksVisableInRenderDistance; yOffset <= chunksVisableInRenderDistance; yOffset++) {
-			for (int xOffset = -chunksVisableInRenderDistance; xOffset <= chunksVisableInRenderDistance; xOffset++) {
+		for (int yOffset = -chunksLoaded; yOffset <= chunksLoaded; yOffset++) {
+			for (int xOffset = -chunksLoaded; xOffset <= chunksLoaded; xOffset++) {
 				Vector2 viewedChunkCoord = new Vector2 (currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
 
 				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
-					terrainChunkDictionary [viewedChunkCoord].UpdateTerrainChunk (renderDistance);
-					if (terrainChunkDictionary [viewedChunkCoord].IsVisible ()) {
+					terrainChunkDictionary [viewedChunkCoord].UpdateChunkState (renderDistance);
+					if (terrainChunkDictionary [viewedChunkCoord].IsRendering ()) {
 						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
 					}
 				} else {
-					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, transform, chunk));
+					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, transform, chunkObject));
 				}
 
 			}
 		}
 	}
 
-	public class TerrainChunk {
+	public class TerrainChunk
+	{
 		GameObject meshObject;
 		Vector2 position;
 		Bounds bounds;
+
 		public TerrainChunk(Vector2 coord, int size, Transform parent, GameObject chunk) {
 			position = coord * size;
 			bounds = new Bounds(position,Vector2.one * size);
 			Vector3 positionV3 = new Vector3(position.x,0,position.y);
 
-			//meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
 			meshObject =  Instantiate(chunk, Vector3.zero, Quaternion.identity);
 			meshObject.transform.position = positionV3;
-			//meshObject.transform.localScale = Vector3.one * size / 10f;
 			meshObject.transform.parent = parent;
-			SetVisible(false);
+			Loader(false);
 		}
 
-		public void UpdateTerrainChunk(float renderDistance) {
-			float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance (viewerPosition));
+		public void UpdateChunkState(float renderDistance)
+		{
+			float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance (center));
 			bool visible = viewerDstFromNearestEdge <= renderDistance;
-			SetVisible (visible);
+			Loader(visible);
 		}
 
-		public void SetVisible(bool visible) {
-			meshObject.SetActive (visible);
-		}
-
-		public bool IsVisible() {
-			return meshObject.activeSelf;
-		}
+		public void Loader(bool visible) { meshObject.SetActive (visible); }
+		public bool IsRendering() { return meshObject.activeSelf; }
 
 	}
 }
