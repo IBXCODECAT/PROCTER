671f0b8a3dfd7199a844cf6c3bd12cba593e7f5d
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-04-15 17:37:54 -0500

    Project not working, attempted data migration

--- Assets/Scripts/ChunkLoader.cs
+++ Assets/Scripts/ChunkLoader.cs
@@ -1,96 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
-public class ChunkLoader : MonoBehaviour {
-	public float renderDistance;
-	public Transform centerReference;
-	[SerializeField] private GameObject chunkObject;
+public class ChunkLoader : MonoBehaviour
+{
+	public static ChunkLoader instance;
 
+	[SerializeField] private GameObject center;
 
-	public static Vector2 center;
-	int chunksLoaded;
-
-	Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
-	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
-
-	void Start() { chunksLoaded = Mathf.RoundToInt(renderDistance / MapGenerator.mapChunkSize); }
-
-	void Update()
+	public void UpdateChunkState(float renderDistance, GameObject chunkObject)
 	{
-		center = new Vector2 (centerReference.position.x, centerReference.position.z);
-		UpdateRenderedOnly();
-	}
-		
-	void UpdateRenderedOnly()
-	{
-		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) { terrainChunksVisibleLastUpdate [i].Loader (false); }
-
-		terrainChunksVisibleLastUpdate.Clear ();
-			
-		int currentChunkCoordX = Mathf.RoundToInt (center.x / MapGenerator.mapChunkSize);
-		int currentChunkCoordY = Mathf.RoundToInt (center.y / MapGenerator.mapChunkSize);
-
-		for (int yOffset = -chunksLoaded; yOffset <= chunksLoaded; yOffset++) {
-			for (int xOffset = -chunksLoaded; xOffset <= chunksLoaded; xOffset++) {
-				Vector2 viewedChunkCoord = new Vector2 (currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
-
-				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
-					terrainChunkDictionary [viewedChunkCoord].UpdateChunkState (renderDistance);
-					if (terrainChunkDictionary [viewedChunkCoord].IsRendering ()) {
-						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
-					}
-				} else {
-					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, MapGenerator.mapChunkSize, transform, chunkObject));
-				}
-
-			}
-		}
+		float viewerDstFromNearestEdge = Mathf.Sqrt(ChunkManagement.Chunk.bounds.SqrDistance(center.transform.position));
+		bool visible = viewerDstFromNearestEdge <= renderDistance;
+		Loader(visible, chunkObject);
 	}
 
-	public class TerrainChunk
-	{
-		GameObject meshObject;
-		Vector2 position;
-		Bounds bounds;
-
-		public TerrainChunk(Vector2 coord, int size, Transform parent, GameObject chunk) {
-
-			Terrain mesh;
-			TerrainData meshData;
-
-			position = coord * size;
-			bounds = new Bounds(position,Vector2.one * size);
-			Vector3 positionV3 = new Vector3(position.x,0,position.y);
-
-			meshObject = Instantiate(chunk, Vector3.zero, Quaternion.identity);
-
-			if (meshObject.GetComponent<Terrain>() == null)
-			{
-				mesh = meshObject.AddComponent<Terrain>();
-			}
-			else
-            {
-				mesh = meshObject.GetComponent<Terrain>();
-            }
-
-			meshData = Data.TerrainDataNew(size, 513, 513, 1024);
-
-			MapGenerator.GenerateMesh(position, meshData);
-
-			meshObject.transform.position = positionV3;
-			meshObject.transform.parent = parent;
-			Loader(false);
-		}
-
-		public void UpdateChunkState(float renderDistance)
-		{
-			float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance (center));
-			bool visible = viewerDstFromNearestEdge <= renderDistance;
-			Loader(visible);
-		}
-
-		public void Loader(bool visible) { meshObject.SetActive (visible); }
-		public bool IsRendering() { return meshObject.activeSelf; }
-
-	}
+	public void Loader(bool visible, GameObject chunkObject) { chunkObject.SetActive(visible); }
+	public bool IsRendering(GameObject chunkObject) { return chunkObject.activeSelf; }
 }
--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,27 +1,50 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public static class Data 
 {
-    private static List<TerrainData> terrainDataMaster = new List<TerrainData>();
+    #region
+    static List<ChunkManagement.Chunk> chunksMaster = new List<ChunkManagement.Chunk>();
+    #endregion
 
-    public static TerrainData TerrainDataNew(int size, short heightMapRes, short baseMapRes, short detailRes)
+    #region
+    static List<Terrain> terrainsMaster = new List<Terrain>();
+    static List<TerrainData> terrainDataMaster = new List<TerrainData>();
+    static List<TerrainCollider> terrainCollidersMaster = new List<TerrainCollider>();
+    #endregion
+
+
+    public struct Chunks
     {
-        TerrainData terrainData = new TerrainData();
-        terrainData.heightmapResolution = size;
-        terrainData.size = new Vector3(2000, 600, 2000);
+        public static ChunkManagement.Chunk[] chunks;
 
-        terrainData.heightmapResolution = 512;
-        terrainData.baseMapResolution = 1024;
-        terrainData.SetDetailResolution(1024, terrainData.detailResolutionPerPatch);
+        public static void AddData(ChunkManagement.Chunk chunk) { chunksMaster.Add(chunk); }
+        public static void RemoveData(ChunkManagement.Chunk chunk) { chunksMaster.Add(chunk); }
+        public static void UpdateData() { chunks = chunksMaster.ToArray(); }
 
-        AssetDatabase.CreateAsset(terrainData, "Assets/Data/New Terrain{0}.asset");
-
-        return terrainData;
     }
 
-    public static void TerrainDataAdd(TerrainData data) { terrainDataMaster.Add(data); }
-    public static void TerrainDataRemove(TerrainData data) { terrainDataMaster.Remove(data); }
-    public static List<TerrainData> TerrainData(TerrainData data) { return terrainDataMaster; }
+    public struct Terrains
+    {
+        public static Terrain[] terrain;
+        public static TerrainData[] terrainData;
+        public static TerrainCollider[] terrainCollider;
+
+        public static void AddData(Terrain data) { terrainsMaster.Add(data); }
+        public static void AddData(TerrainData data) { terrainDataMaster.Add(data); }
+        public static void AddData(TerrainCollider data) { terrainCollidersMaster.Add(data); }
+
+        public static void RemoveData(Terrain data) { terrainsMaster.Remove(data); }
+        public static void RemoveData(TerrainData data) { terrainDataMaster.Remove(data); }
+        public static void RemoveData(TerrainCollider data) { terrainCollidersMaster.Remove(data); }
+
+        public static void UpdateData()
+        {
+            terrain = terrainsMaster.ToArray();
+            terrainData = terrainDataMaster.ToArray();
+            terrainCollider = terrainCollidersMaster.ToArray();
+        }
+    }
 }
