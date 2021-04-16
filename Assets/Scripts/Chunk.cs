4be27c287cc161fbb7cc7f265897300f42f2bec3
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-04-15 20:02:55 -0500

    Seperate Chunk Class

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+	GameObject meshObject;
+	Vector2 position;
+	Bounds bounds;
+
+	public Chunk(Vector2 coord, int size, Transform parent, GameObject chunk)
+	{
+		TerrainData meshData;
+
+		position = coord * size;
+		bounds = new Bounds(position, Vector2.one * size);
+		Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+		meshObject = Instantiate(chunk, Vector3.zero, Quaternion.identity);
+
+		meshData = Data.TerrainDataNew(size, 513, 513, 1024);
+
+		MapGenerator.GenerateMesh(position, meshData);
+
+		meshObject.transform.position = positionV3;
+		meshObject.transform.parent = parent;
+		Loader(false);
+	}
+
+	public void UpdateChunkState(float renderDistance, Vector2 center)
+	{
+		float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(center));
+		bool visible = viewerDstFromNearestEdge <= renderDistance;
+		Loader(visible);
+	}
+
+	public void Loader(bool visible) { meshObject.SetActive(visible); }
+	public bool IsRendering() { return meshObject.activeSelf; }
+
+}
---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ChunkManager : MonoBehaviour {
+	public float renderDistance;
+	public Transform centerReference;
+	[SerializeField] private GameObject chunkObject;
+
+	public static Vector2 center;
+
+	int chunksLoaded;
+
+	Dictionary<Vector2, Chunk> terrainChunkDictionary = new Dictionary<Vector2, Chunk>();
+	List<Chunk> terrainChunksVisibleLastUpdate = new List<Chunk>();
+
+	void Start() { chunksLoaded = Mathf.RoundToInt(renderDistance / MapGenerator.mapChunkSize); }
+
+	void Update()
+	{
+		center = new Vector2 (centerReference.position.x, centerReference.position.z);
+		UpdateRenderedOnly();
+	}
+		
+	void UpdateRenderedOnly()
+	{
+		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) { terrainChunksVisibleLastUpdate [i].Loader (false); }
+
+		terrainChunksVisibleLastUpdate.Clear ();
+			
+		int currentChunkCoordX = Mathf.RoundToInt (center.x / MapGenerator.mapChunkSize);
+		int currentChunkCoordY = Mathf.RoundToInt (center.y / MapGenerator.mapChunkSize);
+
+		for (int yOffset = -chunksLoaded; yOffset <= chunksLoaded; yOffset++) {
+			for (int xOffset = -chunksLoaded; xOffset <= chunksLoaded; xOffset++) {
+				Vector2 viewedChunkCoord = new Vector2 (currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
+					terrainChunkDictionary [viewedChunkCoord].UpdateChunkState (renderDistance, center);
+					if (terrainChunkDictionary [viewedChunkCoord].IsRendering ()) {
+						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
+					}
+				} else {
+					terrainChunkDictionary.Add (viewedChunkCoord, new Chunk (viewedChunkCoord, MapGenerator.mapChunkSize, transform, chunkObject));
+				}
+			}
+		}
+	}
+}
